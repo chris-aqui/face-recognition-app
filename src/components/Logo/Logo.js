3543ce887626c0cb20c21c3877262c25e2689cf7
christine-aqui <2739361+christine-aqui@users.noreply.github.com>
2018-03-24 23:14:55 -0400

    code along for section 22, L166

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tilt from 'react-tilt'
 import './Logo.css';
-import brain from './brain-100 (1).png';
+import brain from './brain-100.png';
 
 const Logo = () => {
   return (
