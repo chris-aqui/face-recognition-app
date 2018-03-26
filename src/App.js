fde034fbff28cbf85e0dc6e895c5c41eaefaa41a
christine-aqui <2739361+christine-aqui@users.noreply.github.com>
2018-03-25 20:21:00 -0400

    code along
    api intergration for face detection

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Rank from "./components/Rank/Rank";
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'ec277d8de0eb4afb889a220a80d64c7f'
+ });
 
 const particlesOptions = {
   particles: {
@@ -12,13 +18,43 @@ const particlesOptions = {
       value: 50,
       density: {
         enable: true,
-        value_area: 800
+        value_area: 600
       }
     }
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      input:'',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) =>{
+    // console.log(event.target.value);
+    //this updates
+    this.setState({input:event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    // console.log("click");
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,8 +64,8 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-         {/*<FaceRecognition /> */}
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
